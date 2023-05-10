86b6c5f35ed19ca8f96eddb93b71f43bad32ba37
kitminsk <kitminsk@gmail.com>
2023-05-10 12:04:21 +0200

    Task 1

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const miniCss = require('mini-css-extract-plugin');
+const HtmlCriticalWebpackPlugin = require("html-critical-webpack-plugin");
 
 module.exports = {
     mode: 'production',
     entry: path.resolve(__dirname, 'src/index.js'),
     output: {
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
     },
     module: {
@@ -33,10 +35,28 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./src/index.html"
+            template: path.resolve(__dirname, 'src/index.html'),
+            filename: 'index.html',
+            inject: true,
         }),
         new miniCss({
             filename: 'style.css',
         }),
+        new HtmlCriticalWebpackPlugin({
+            base: path.resolve(__dirname, 'dist'),
+            src: 'index.html',
+            dest: 'index.html',
+            inline: true,
+            minify: true,
+            extract: true,
+            dimensions: [
+                { width: 320, height: 480 }, // iPhone 5
+                { width: 768, height: 1024 }, // iPad
+                { width: 1920, height: 1080 }, // Desktop
+            ],
+            penthouse: {
+                blockJSRequests: false,
+            }
+        })
     ],
 };
